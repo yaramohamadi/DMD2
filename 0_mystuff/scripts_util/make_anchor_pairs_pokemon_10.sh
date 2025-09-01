787b76e89b6f2145bef8d2681b14cadd3a043bde
yaramohamadi <yara.mohamadi@gmail.com>
2025-09-01 14:48:18 -0400

    rename folders

--- 0_mystuff/scripts_util/make_anchor_pairs_pokemon_10.sh
+++
@@ -1,5 +0,0 @@
-python 0_mystuff/scripts_util/make_anchor_pairs.py \
-  --real_image_path 0_mystuff/checkpoint_path/pokemon_lmdb \
-  --out_path 0_mystuff/checkpoint_path/pokemon_lmdb/pokemon_1000_anchors.pt \
-  --label_dim 1000 --resolution 64 --conditioning_sigma 80.0 \
-  --anchors_per_image 1000
