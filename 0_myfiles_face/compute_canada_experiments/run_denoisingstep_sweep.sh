e0182f30f1633d84544f437974db452cef5800b2
yaramohamadi <yara.mohamadi@gmail.com>
2025-09-13 15:41:39 -0400

    New ablation LR and CLS loss new setting BS8DN4DMD1

--- 0_myfiles_face/compute_canada_experiments/run_denoisingstep_sweep.sh
+++ 0_myfiles_face/compute_canada_experiments/run_denoisingstep_sweep.sh
@@ -1,40 +1,42 @@
 #!/bin/bash
-# 
+
 CHILD="0_myfiles_face/compute_canada_experiments/run_config_babies.sh"       # <-- point to the sbatch file above
 LOGDIR="0_myfiles_face/slurm"
 mkdir -p "$LOGDIR"
 
-# CUDA_VISIBLE_DEVICES=0,TRAIN_GPUS=0,TEST_GPUS=0,NPROC_PER_NODE=1,NNODES=1
-
 # pick the sweep you want
-STEPS=(4)
+
+GEN_CLS_LOSS_WEIGHT=(3e-3 15e-3)
+CLS_LOSS_WEIGHT=(1e-3 5e-3)
+GEN_LR=(5e-8)
+
+export WANDB_PROJECT=DMD_ABLATE_LR_CLSLOSS"}"
 
 for dn in "${STEPS[@]}"; do
-  tag="dn${dn}"
-  sbatch \
-    --job-name="dmd2_babies_${tag}" \
-    --output="${LOGDIR}/dmd2_babies_${tag}-%j.out" \
-    --error="${LOGDIR}/dmd2_babies_${tag}-%j.err" \
-    --export=ALL,NUM_DENOISING_STEP="$dn",EXTRA_TAG="_${tag}" \
-    "$CHILD"
+  for dn in "${STEPS[@]}"; do
+    tag="dn${dn}"
+    sbatch \
+      --job-name="dmd2_babies_${tag}" \
+      --output="${LOGDIR}/dmd2_babies_${tag}-%j.out" \
+      --error="${LOGDIR}/dmd2_babies_${tag}-%j.err" \
+      --export=ALL,EXTRA_TAG="_${tag}" \
+      "$CHILD"
+  done
 done
 
-#!/bin/bash
 
-# CHILD="0_myfiles_face/compute_canada_experiments/run_config_babies.sh"
+
+# #!/bin/bash
+# 
+# CHILD="0_myfiles_face/compute_canada_experiments/run_config_babies.sh"   # point to your script
 # 
 # STEPS=(4)
 # 
-# export CUDA_VISIBLE_DEVICES=0 # ,1,2,3,4,5,6,7
-# export TRAIN_GPUS=0 # ,1,2,3,4,5,6,7
-# export TEST_GPUS=0 # 7
-# export NPROC_PER_NODE=1 #8
-# export NNODES=1
-# 
-# 
 # for dn in "${STEPS[@]}"; do
+#   tag="dn${dn}"
+#   echo "Running with NUM_DENOISING_STEP=$dn"
 # 
-#   echo "[*] Launch ${tag} -> ${out}"
-#   export NUM_DENOISING_STEP="$dn" 
-#   bash "$CHILD"
+#   # Run locally, export variables, redirect logs
+#   NUM_DENOISING_STEP="$dn" EXTRA_TAG="$tag" bash "$CHILD"
 # done
+
