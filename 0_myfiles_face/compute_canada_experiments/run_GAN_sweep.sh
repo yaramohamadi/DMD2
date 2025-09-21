5cd975d1e68886df872e248cc0a1c400d6143555
Yara Bahram <yara.mohamadi@gmail.com>
2025-09-21 17:19:35 -0400

    yes

--- 0_myfiles_face/compute_canada_experiments/run_GAN_sweep.sh
+++ 0_myfiles_face/compute_canada_experiments/run_GAN_sweep.sh
@@ -4,13 +4,12 @@ CHILD="0_myfiles_face/compute_canada_experiments/run_config_babies.sh"
 SLURM_LOG_DIR="0_myfiles_face/slurm"
 mkdir -p "$SLURM_LOG_DIR"
 
-LOSSES=("hinge" "lsgan")
+LOSSES=("lsgan") # "hinge" 
 
 WANDB_PROJECT="DMD_babies_gan_sweep"
 
 for loss in "${LOSSES[@]}"; do
   tag="gan_${loss}"
-
 
   if [[ -n "$SLURM_JOB_ID" ]]; then
     # HPC / SLURM
@@ -23,13 +22,14 @@ for loss in "${LOSSES[@]}"; do
   else
     # Local run
     echo "[LOCAL] running GAN loss = $loss"
+    export CHECKPOINT_PATH="0_myfiles_face/checkpoint_path/babies_lr5e-8_bs1_dn2_DMD1_GClsw15e-3__gan_lsgan/checkpoint_model_031900"
     export GAN_ADV_LOSS="$loss" 
     export WANDB_PROJECT="$WANDB_PROJECT" 
     export EXTRA_TAG="_${tag}" 
     export CUDA_VISIBLE_DEVICES=0,1
-    export TRAIN_GPUS=0,1
+    export TRAIN_GPUS=0
     export TEST_GPUS=1
-    export NPROC_PER_NODE=2
+    export NPROC_PER_NODE=1
     export NNODES=1
     bash "$CHILD"
   fi
