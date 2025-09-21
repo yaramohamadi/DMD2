61c3797835a71364c5e305d4e7abbefeb585a34a
yaramohamadi <yara.mohamadi@gmail.com>
2025-09-21 15:35:41 -0400

    sunglasses ok

--- 0_myfiles_face/compute_canada_experiments/run_sunglasses.sh
+++ 0_myfiles_face/compute_canada_experiments/run_sunglasses.sh
@@ -4,6 +4,7 @@ CHILD="0_myfiles_face/compute_canada_experiments/run_config_babies.sh"       # <
 LOGDIR="0_myfiles_face/slurm"
 mkdir -p "$LOGDIR"
 
+
 GEN_CLS_LOSS_WEIGHT=(15e-3)
 CLS_LOSS_WEIGHT=(5e-2)
 GEN_LR=(5e-8)
@@ -19,6 +20,7 @@ for lr in "${GEN_LR[@]}"; do
 
     echo "[LOCAL] lr=$lr  GEN_CLS_LOSS_WEIGHT=$glw  CLS_LOSS_WEIGHT=$clw  tag=$tag"
 
+    export CHECKPOINT_PATH = "0_myfiles_face/checkpoint_path/metfaces_lr5e-8_bs1_dn3_DMD1_GClsw15e-3__lr5e-8_clw5e-2_glw15e-3/checkpoint_model_015000"
     export DATASET_NAME="sunglasses"
     export GEN_LR="$lr" 
     export GEN_CLS_LOSS_WEIGHT="$glw" 
