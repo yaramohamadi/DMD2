e71d0089cafed66bbb44411008d32534da04bfc1
yaramohamadi <yara.mohamadi@gmail.com>
2025-09-21 17:45:40 -0400

    minor bug fixes for CATS

--- 0_myfiles_face/compute_canada_experiments/run_both.sh
+++ 0_myfiles_face/compute_canada_experiments/run_both.sh
@@ -46,9 +46,12 @@ train() {
       --gan_head_layers "$GAN_HEAD_LAYERS" \
       --gan_adv_loss "$GAN_ADV_LOSS" \
       $USE_BF16 \
-      --grad_accum_steps "$GRAD_ACCUM_STEPS" \
-      --checkpoint_path "$CHECKPOINT_PATH"
+      --grad_accum_steps "$GRAD_ACCUM_STEPS"
 }
+
+#  \
+#      --checkpoint_path "$CHECKPOINT_PATH"
+
 
 # -----------------------
 # Testing (streaming conditional)
@@ -108,14 +111,14 @@ test_null() {
 # -----------------------
 # Orchestration
 # -----------------------
-#train &                  # start training
-#TRAIN_PID=$!
+train &                  # start training
+TRAIN_PID=$!
 test_stream_conditional &  # start streaming conditional eval
 TEST_PID=$!
 
 # Wait for both to finish 
 wait $TEST_PID
-#wait $TRAIN_PIDs
+wait $TRAIN_PIDs
 
 # After training finishes, evaluate best checkpoint with NULL sampling
 # test_best_null
