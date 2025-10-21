20f0217bd4f4f0371b0f78c3a21aa705f5e625c5
Yara Bahram <yara.mohamadi@gmail.com>
2025-10-21 10:10:27 -0400

    refactor small

--- 0_myfiles_face/compute_canada_experiments/run_GAN_sweep.sh
+++ 0_myfiles_face/compute_canada_experiments/run_GAN_sweep.sh
@@ -4,11 +4,11 @@ CHILD="0_myfiles_face/compute_canada_experiments/run_config_babies.sh"
 SLURM_LOG_DIR="0_myfiles_face/slurm"
 mkdir -p "$SLURM_LOG_DIR"
 
-LOSSES=("bce" "lsgan" "hinge" "wgan")  #  "wgan"
+LOSSES=("hinge")  #  "wgan"
 
 export NO_LPIPS="--no_lpips"
 
-WANDB_PROJECT="DMD_babies_gan_sweep"
+WANDB_PROJECT="DMD_sunglasses_gan_sweep"
 
 for loss in "${LOSSES[@]}"; do
   tag="gan_${loss}"
@@ -16,22 +16,23 @@ for loss in "${LOSSES[@]}"; do
   if [[ -n "$SLURM_JOB_ID" ]]; then
     # HPC / SLURM
     sbatch \
-      --job-name="dmd2_babies_${tag}" \
-      --output="${SLURM_LOG_DIR}/dmd2_babies_${tag}-%j.out" \
-      --error="${SLURM_LOG_DIR}/dmd2_babies_${tag}-%j.err" \
+      --job-name="dmd2_sunglasses_${tag}" \
+      --output="${SLURM_LOG_DIR}/dmd2_sunglasses_${tag}-%j.out" \
+      --error="${SLURM_LOG_DIR}/dmd2_sunglasses_${tag}-%j.err" \
       --export=ALL,GAN_ADV_LOSS="$loss",WANDB_PROJECT="$WANDB_PROJ",EXTRA_TAG="_${tag}" \
       "$CHILD"
   else
     # Local run
     echo "[LOCAL] running GAN loss = $loss"
-    export DATASET_NAME="babies"
+    export DATASET_NAME="sunglasses"
     export GAN_ADV_LOSS="$loss" 
     export WANDB_PROJECT="$WANDB_PROJECT" 
     export EXTRA_TAG="_${tag}" 
     export CUDA_VISIBLE_DEVICES=0,1
-    export TRAIN_GPUS=0
-    export TEST_GPUS=1
+    export TRAIN_GPUS=0,1
+    export TEST_GPUS=2
     export NPROC_PER_NODE=1
+    export GRAD_ACCUM_STEPS=2
     export NNODES=1
     bash "$CHILD"
   fi
