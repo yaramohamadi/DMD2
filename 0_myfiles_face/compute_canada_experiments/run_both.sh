5811c700d5112a240cf652bec51dae2ff3610f8b
Yara Bahram <yara.mohamadi@gmail.com>
2025-10-23 00:34:18 -0400

    fix

--- 0_myfiles_face/compute_canada_experiments/run_both.sh
+++ 0_myfiles_face/compute_canada_experiments/run_both.sh
@@ -48,7 +48,8 @@ train() {
       --gan_adv_loss "${GAN_ADV_LOSS:-}" \
       ${USE_BF16:-} \
       --grad_accum_steps "${GRAD_ACCUM_STEPS:-1}" \
-      ${REVERSE_DMD:-}
+      ${REVERSE_DMD:-} \
+      ${TRAIN_FAKE_ON_REAL:-}
 }
 
 # -----------------------
