3e9f1429d333e54eb9c424434dff643c39458b45
Yara Bahram <yara.mohamadi@gmail.com>
2026-01-27 23:48:29 -0500

    minor

--- 0_myfiles_face/scripts/checkpoint_finetuned.sh
+++ 0_myfiles_face/scripts/checkpoint_finetuned.sh
@@ -1,6 +1,6 @@
 python 0_myfiles_face/scripts/checkpoint_distill_finetuned.py \
-  --ckpt 0_myfiles_face/checkpoint_path/babies_finetune/checkpoint_best/pytorch_model.bin \
-  --out 0_myfiles_face/checkpoints/sunglasses_finetune.pt \
+  --ckpt 0_myfiles_face/checkpoint_path/babies_lr2e-7_bs1_dn3_DMD0_GClsw15e-3__naive_all_dataset1_ftnaive_ddpmall/checkpoint_best/pytorch_model.bin \
+  --out 0_myfiles_face/checkpoints/babies1shot_finetune.pt \
   --include-prefix unet. \
   --strip-prefix unet. \
   --show-prefixes
